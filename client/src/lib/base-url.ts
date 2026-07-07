787acf3dda52c22d656591b555a2d52ba414ee61
abinesh2323 <abineshkalyan2002@gmail.com>
2026-07-07 15:41:29 +0530

    new appsail link

--- client/src/lib/base-url.ts
+++ client/src/lib/base-url.ts
@@ -1,1 +1,1 @@
-export const baseURL = "https://aa-10127368540.development.catalystappsail.com/api";
+export const baseURL = "https://officehubtech2-10127368540.development.catalystappsail.com/api";
